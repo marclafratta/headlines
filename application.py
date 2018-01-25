1f4dfa74d5232f1017a8c6dee4335d7240eca654
marclafratta <marclafratta@gmail.com>
2018-01-25 17:50:01 -0500

    finish app, adding icon

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import feedparser
 import json
 from urllib.request import urlopen
 import urllib
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 application = Flask(__name__)
 
@@ -15,7 +15,13 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'torstar': 'http://www.thestar.com/feeds.topstories.rss'}
 
 DEFAULTS = {'publication': 'ctv',
-            'city': 'Toronto'}
+            'city': 'Toronto',
+            'currency_from': 'CAD',
+            'currency_to': 'USD'}
+
+# API Urls
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=35a35cfa5c2355830abde9fe2c1896d0"
+CURRENCY_URL = "http://openexchangerates.org//api/latest.json?app_id=497a7259f46147d58a2821d70f73d21f"
 
 @application.route("/")
 def get_news():
@@ -35,7 +41,17 @@ def get_news():
 
     weather = get_weather(city)
 
-    return render_template("home.html", articles=articles, weather=weather)
+    # get customized currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+
+    return render_template("home.html", articles=articles, weather=weather, currency_from=currency_from,
+                           currency_to=currency_to, rate=rate, currencies=sorted(currencies))
 
 
 def get_news(query):
@@ -48,16 +64,30 @@ def get_news(query):
 
 
 def get_weather(query):
-    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=35a35cfa5c2355830abde9fe2c1896d0"
     query = urllib.parse.quote(query)
-    url = api_url.format(query)
+    url = WEATHER_URL.format(query)
     data = urlopen(url).read()
     parsed = json.loads(data)
     weather = None
     if parsed.get("weather"):
-        weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]
-                  }
+        weather = {"description": parsed["weather"][0]["description"], "temperature": parsed["main"]["temp"],
+                   "city": parsed["name"], 'country': parsed['sys']['country']
+                   }
     return weather
+
+
+def get_rate(frm, to):
+    all_currency = urlopen(CURRENCY_URL).read()
+
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate / frm_rate, parsed.keys())
+
+
+@application.route('/favicon.ico')
+def hello():
+    return redirect(url_for('static', filename='favicon.ico'), code=302)
 
 
 if __name__ == '__main__':
