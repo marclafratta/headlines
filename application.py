aa33a47bcfbea971484e55abf3ac035c197522a8
marclafratta <marclafratta@gmail.com>
2018-01-24 21:55:41 -0500

    adding ctv

--- application.py
+++ application.py
@@ -7,12 +7,13 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za.ezproxy.torontopubliclibrary.ca/cmlink/1.640',
-             'cbc': 'http://rss.cbc.ca/lineup/topstories.xml'}
+             'cbc': 'http://rss.cbc.ca/lineup/topstories.xml',
+             'ctv': 'http://toronto.ctvnews.ca/rss/Latest'}
 
 
 @application.route("/")
 @application.route("/<publication>")
-def get_news(publication = "cbc"):
+def get_news(publication = "ctv"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
     return render_template("home.html", articles=feed['entries'])
