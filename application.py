7d334b359d1ce0375242cc06233ee0db67bc0869
marclafratta <marclafratta@gmail.com>
2018-01-25 14:37:07 -0500

    adding weather

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
 import feedparser
+import json
+from urllib.request import urlopen
+import urllib
 from flask import Flask, render_template, request
 
 application = Flask(__name__)
@@ -8,20 +11,34 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za.ezproxy.torontopubliclibrary.ca/cmlink/1.640',
              'cbc': 'http://rss.cbc.ca/lineup/topstories.xml',
-             'ctv': 'http://ctvnews.ca/rss/TopStories'}
+             'torstar': 'http://www.thestar.com/feeds.topstories.rss'}
 
 
 @application.route("/")
 def get_news():
     query = request.args.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
-        publication = "ctv"
+        publication = "torstar"
     else:
         publication = query.lower()
 
     feed = feedparser.parse(RSS_FEEDS[publication])
+    weather = get_weather("Toronto")
 
-    return render_template("home.html", articles=feed['entries'])
+    return render_template("home.html", articles=feed['entries'], weather=weather)
+
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=35a35cfa5c2355830abde9fe2c1896d0"
+    query = urllib.parse.quote(query)
+    url = api_url.format(query)
+    data = urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":parsed["weather"][0]["description"],"temperature":parsed["main"]["temp"],"city":parsed["name"]
+                  }
+    return weather
 
 
 if __name__ == '__main__':
